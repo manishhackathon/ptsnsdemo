b7bc0018cb23f551f853ec9f2ad5bd3232c4b0dc
ManishHackathon <manishhackathon@gmail.com>
2026-01-16 18:37:32 +0000

    i have replaced the header background image and it is not reflecting

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,30 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: '/_next/image',
+        has: [
+          {
+            type: 'query',
+            key: 'url',
+            value: '/header.png',
+          },
+        ],
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+      {
         source: '/header.png',
         headers: [
           {
