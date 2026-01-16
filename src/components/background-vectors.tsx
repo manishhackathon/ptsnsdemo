a28a56dc0783c6b106d197d31e6437c73c1d8f14
ManishHackathon <manishhackathon@gmail.com>
2026-01-16 20:00:38 +0000

    pls increase the vectors more and now all the vectors in deep red color

--- src/components/background-vectors.tsx
+++ src/components/background-vectors.tsx
@@ -12,7 +12,7 @@ const BackgroundVectors = () => {
             width="500"
             height="700"
             patternUnits="userSpaceOnUse"
-            patternTransform="scale(0.6) rotate(5)"
+            patternTransform="scale(0.5) rotate(5)"
           >
             <rect width="500" height="700" fill="transparent" />
             
@@ -73,6 +73,35 @@ const BackgroundVectors = () => {
             <path d="M 150 600 q 50 -50 100 0" stroke="hsl(var(--vector-stroke-left))" fill="none" strokeWidth="2" />
             <path d="M 20 500 q 30 -30 60 0" stroke="hsl(var(--vector-stroke-left))" fill="none" strokeWidth="2" />
             <path d="M 350 650 q 20 -20 40 0" stroke="hsl(var(--vector-stroke-left))" fill="none" strokeWidth="2" />
+
+            {/* EVEN MORE STUFF FOR DENSITY */}
+            <g transform="translate(350, 200) scale(0.8)">
+              <circle cx="10" cy="5" r="5" fill="hsl(var(--vector-fill-left))" />
+              <line x1="10" y1="10" x2="10" y2="30" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2" />
+              <line x1="10" y1="20" x2="0" y2="25" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2" />
+              <line x1="10" y1="20" x2="20" y2="25" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2" />
+            </g>
+            <g transform="translate(50, 250) scale(1.0)">
+                <path d="M 0 20 C 10 0, 30 0, 40 20" stroke="hsl(var(--vector-stroke-left))" fill="none" strokeWidth="2" />
+                <line x1="20" y1="20" x2="20" y2="35" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2" />
+            </g>
+            <g transform="translate(180, 280) rotate(30) scale(0.7)">
+              <path d="M 0 20 C -20 10, -20 -10, 0 -20 C 20 -10, 20 10, 0 20 Z" fill="hsl(var(--vector-fill-left))" />
+            </g>
+            <circle cx="480" cy="400" r="12" fill="none" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2"/>
+            <path d="M 20 650 q 50 30 100 0" stroke="hsl(var(--vector-stroke-left))" fill="none" strokeWidth="2" />
+            <path d="M 120 50 q 20 -20 40 0" stroke="hsl(var(--vector-stroke-left))" fill="none" strokeWidth="2" />
+            <path d="M 400 600 l 15 0 l -7 -12 z" fill="hsl(var(--vector-fill-left))" />
+            <g transform="translate(300, 600) scale(1.3)">
+              <circle cx="0" cy="0" r="20" fill="none" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2"/>
+              <line x1="0" y1="-25" x2="0" y2="25" stroke="hsl(var(--vector-stroke-left))" strokeWidth="1"/>
+              <line x1="-25" y1="0" x2="25" y2="0" stroke="hsl(var(--vector-stroke-left))" strokeWidth="1"/>
+            </g>
+            <g transform="translate(150, 500) scale(0.9) rotate(-30)">
+              <path d="M 0 20 C 10 0, 30 0, 40 20 C 50 40, 60 40, 70 20" stroke="hsl(var(--vector-stroke-left))" fill="none" strokeWidth="3" />
+              <line x1="20" y1="20" x2="20" y2="40" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2" />
+              <line x1="50" y1="20" x2="50" y2="40" stroke="hsl(var(--vector-stroke-left))" strokeWidth="2" />
+            </g>
           </pattern>
 
           {/* Right side pattern: Tiger & Millets Theme */}
@@ -81,7 +110,7 @@ const BackgroundVectors = () => {
             width="500"
             height="700"
             patternUnits="userSpaceOnUse"
-            patternTransform="scale(0.6) rotate(-5)"
+            patternTransform="scale(0.5) rotate(-5)"
           >
             <rect width="500" height="700" fill="transparent" />
             
@@ -147,6 +176,34 @@ const BackgroundVectors = () => {
               <path d="M 0 -25 L 5 -35 L -5 -35 Z" fill="hsl(var(--vector-stroke-right))" />
               <path d="M 0 25 L 5 35 L -5 35 Z" fill="hsl(var(--vector-stroke-right))" transform="rotate(180)" />
             </g>
+
+            {/* EVEN MORE STUFF FOR DENSITY */}
+            <g transform="translate(150, 220) rotate(90)">
+              <path d="M 0 0 Q 20 50, 40 0" stroke="hsl(var(--vector-stroke-right))" strokeWidth="7" fill="none" />
+              <path d="M 20 20 Q 40 70, 60 20" stroke="hsl(var(--vector-stroke-right))" strokeWidth="7" fill="none" />
+            </g>
+            <g transform="translate(20, 250) scale(1.1)">
+                <circle cx="0" cy="0" r="3" fill="hsl(var(--vector-fill-right))" />
+                <circle cx="10" cy="5" r="3" fill="hsl(var(--vector-fill-right))" />
+                <circle cx="-5" cy="12" r="3" fill="hsl(var(--vector-fill-right))" />
+                <circle cx="8" cy="18" r="3" fill="hsl(var(--vector-fill-right))" />
+                <circle cx="15" cy="25" r="3" fill="hsl(var(--vector-fill-right))" />
+            </g>
+            <g transform="translate(450, 50) scale(0.9)">
+                <circle cx="0" cy="0" r="3" fill="hsl(var(--vector-fill-right))" />
+                <circle cx="-10" cy="5" r="3" fill="hsl(var(--vector-fill-right))" />
+                <circle cx="5" cy="12" r="3" fill="hsl(var(--vector-fill-right))" />
+            </g>
+            <g transform="translate(450, 450)">
+              <circle cx="0" cy="0" r="25" fill="none" stroke="hsl(var(--vector-stroke-right))" strokeWidth="2" />
+              <path d="M 0 -30 L 5 -40 L -5 -40 Z" fill="hsl(var(--vector-stroke-right))" />
+              <path d="M 0 30 L 5 40 L -5 40 Z" fill="hsl(var(--vector-stroke-right))" transform="rotate(180)" />
+            </g>
+            <g transform="translate(180, 580) rotate(-45)">
+              <path d="M 0 0 Q 15 40, 30 0" stroke="hsl(var(--vector-stroke-right))" strokeWidth="4" fill="none" />
+              <path d="M 25 0 Q 40 40, 55 0" stroke="hsl(var(--vector-stroke-right))" strokeWidth="4" fill="none" />
+            </g>
+
           </pattern>
         </defs>
 
