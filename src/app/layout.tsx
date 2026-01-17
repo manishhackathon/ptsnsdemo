ff6ac5cb70ac61e9db94246a6289e7423bffe7c4
ManishHackathon <manishhackathon@gmail.com>
2026-01-17 05:37:54 +0000

    i meant university name's font color not its background color, please us

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Noto+Sans+Devanagari:wght@400;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-white">
+      <body className="font-body antialiased bg-background">
         <div className="relative z-10 mx-auto flex min-h-screen max-w-[95%] flex-col border border-accent/50 bg-muted shadow-2xl">
           <Header />
           <NewsTicker />
